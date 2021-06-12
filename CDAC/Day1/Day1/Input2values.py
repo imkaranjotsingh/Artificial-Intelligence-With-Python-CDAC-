08100e46870254fa6a9a02bc9eb848dbc7fdadab
Karanjot Singh <karanjot5665@gmail.com>
2021-06-13 03:29:11 +0530

    Delete CDAC directory

--- CDAC/Day1/Day1/Input2values.py
+++
@@ -1,4 +0,0 @@
-first_num = input("Enter the first number:")
-second_num = input("Enter the second number:")
-c = int(first_num) + int(second_num)
-print(c)
--- CDAC/Day1/Day1/swaping.py
+++
@@ -1,9 +0,0 @@
-a = 10
-b = 44
-print("Value Of A Before Swaping:",a)
-print("Value of B Before Swaping:",b)
-c = a
-a = b
-b = c
-print("Value Of A After Swaping:",a)
-print("Value of B After Swaping:",b)
